8ec394495cf6a3d129bd97686b6e4010b36eaea6
Richa <richa.bisht@suse.com>
2024-10-16 11:33:06 +0200

    Fixed lint and added changelog files, license fix, test fixes

--- web/html/src/manager/content-management/list-filters/list-filters.tsx
+++ web/html/src/manager/content-management/list-filters/list-filters.tsx
@@ -164,7 +164,6 @@ const ListFilters = (props: Props) => {
       button={panelButtons}
       helpUrl="reference/clm/clm-filters.html"
     >
-      test
       <Table
         data={displayedFilters}
         identifier={identifier}
