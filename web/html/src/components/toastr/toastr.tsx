a99b4bc1f62870e449a1db4560d9da120675f538
Richa <richa.bisht@suse.com>
2025-11-11 17:05:51 +0100

    Clean code

--- web/html/src/components/toastr/toastr.tsx
+++ web/html/src/components/toastr/toastr.tsx
@@ -5,12 +5,10 @@ import { cssTransition, toast, ToastContainer } from "react-toastify";
 type OptionalParams = {
   autoHide: boolean;
   containerId?: string;
-  closeButton?: boolean;
 };
 
 type MessagesContainerProps = {
   containerId?: string;
-  closeButton?: boolean;
 };
 
 const FadeTransition = cssTransition({
@@ -42,7 +40,6 @@ export function showSuccessToastr(message: React.ReactNode, optionalParams: Opti
     toast.success(msg, {
       autoClose: parseAutoHide(optionalParams.autoHide),
       containerId: optionalParams.containerId,
-      closeButton: optionalParams.closeButton,
     });
 
   show(message, notify);
