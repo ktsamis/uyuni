88f3245801383150e6e3ea55e51ebe03f926cccb
Ondrej Holecek <oholecek@suse.com>
2025-12-01 13:24:45 +0100

    Review fixes

--- java/code/src/com/suse/manager/saltboot/test/SaltbootMigrationUtilsTest.java
+++ java/code/src/com/suse/manager/saltboot/test/SaltbootMigrationUtilsTest.java
@@ -46,6 +46,7 @@ public class SaltbootMigrationUtilsTest extends JMockBaseTestCaseWithUser {
 
     private CobblerConnection client;
 
+    @Override
     @BeforeEach
     public void setUp() throws Exception {
         super.setUp();
@@ -53,8 +54,9 @@ public class SaltbootMigrationUtilsTest extends JMockBaseTestCaseWithUser {
         client = new MockConnection("http://localhost", "token");
     }
 
+    @Override
     @AfterEach
-    public void teardown() throws Exception {
+    public void tearDown() throws Exception {
         MockConnection.clear();
         super.tearDown();
     }
