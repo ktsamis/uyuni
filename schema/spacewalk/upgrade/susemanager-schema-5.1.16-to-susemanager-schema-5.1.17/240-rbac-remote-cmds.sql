9de98bd2be38278950efde82886714018752733f
Can Bulut Bayburt <cbbayburt@suse.com>
2026-04-09 17:46:00 +0200

    Move Salt > Remote Commands to RBAC namespace 'salt.remote_commands' (bsc#1261305)

---
+++ schema/spacewalk/upgrade/susemanager-schema-5.1.16-to-susemanager-schema-5.1.17/240-rbac-remote-cmds.sql
@@ -0,0 +1,26 @@
+--
+-- Copyright (c) 2026 SUSE LLC
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+
+INSERT INTO access.namespace (namespace, access_mode, description)
+    SELECT 'salt.remote_commands', 'W', 'Execute remote commands on systems'
+    WHERE NOT EXISTS (SELECT 1 FROM access.namespace WHERE namespace = 'salt.remote_commands' AND access_mode = 'W');
+
+UPDATE access.endpointNamespace
+SET namespace_id = (SELECT id FROM access.namespace WHERE namespace = 'salt.remote_commands' AND access_mode = 'W')
+WHERE endpoint_id = (SELECT id FROM access.endpoint WHERE endpoint = '/manager/systems/cmd' AND http_method = 'GET');
+
+-- Permit to all access groups
+INSERT INTO access.accessGroupNamespace
+    SELECT ag.id, ns.id
+    FROM access.accessGroup ag, access.namespace ns
+    WHERE ns.namespace = 'salt.remote_commands'
+    AND ns.access_mode = 'W'
+    ON CONFLICT DO NOTHING;
