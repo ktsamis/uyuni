ad6ddcbbc60ecea25bd4a0bae92668bb3b3364df
Konstantinos Tsamis <ktsamis@suse.com>
2026-05-19 17:08:40 +0200

    Add rhel10 clones to BV

--- testsuite/features/support/commonlib.rb
+++ testsuite/features/support/commonlib.rb
@@ -942,6 +942,7 @@ end
 # @param hostname String The hostname of the requested system
 # @param package String The package name where it will trigger an upgrade
 def trigger_upgrade(hostname, package)
+  get_target('server').run('spacecmd -u admin -p admin clear_caches', check_errors: false)
   get_target('server').run("spacecmd -u admin -p admin system_upgradepackage #{hostname} #{package} -y", check_errors: true)
 end
 
@@ -950,6 +951,7 @@ end
 # @param hostname String The hostname of the requested system
 # @param package String The package name to install
 def trigger_install(hostname, package)
+  get_target('server').run('spacecmd -u admin -p admin clear_caches', check_errors: false)
   get_target('server').run("spacecmd -u admin -p admin system_installpackage #{hostname} #{package} -y", check_errors: true)
 end
 
@@ -958,6 +960,7 @@ end
 # @param hostname String The hostname of the requested system
 # @param package String The package name to remove
 def trigger_remove(hostname, package)
+  get_target('server').run('spacecmd -u admin -p admin clear_caches', check_errors: false)
   get_target('server').run("spacecmd -u admin -p admin system_removepackage #{hostname} #{package} -y", check_errors: true)
 end
 
