faad9a94a65a0c261dd02e3837c3aba62ba05814
Ondrej Holecek <oholecek@suse.com>
2026-05-26 09:56:53 +0200

    Correct license case and add copyright info

--- containers/server-postgresql-image/root/usr/bin/sanity_run.sh
+++ containers/server-postgresql-image/root/usr/bin/sanity_run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2026 SUSE LLC
+#
 # SPDX-License-Identifier: MIT
+
 # Sanity checks run as root before privilege drop.
 # Fixes ownership/permission issues from legacy installations.
 
